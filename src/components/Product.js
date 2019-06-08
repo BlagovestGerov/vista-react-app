20df116db9e9646338ac854efa02f0e174279396
BlagovestGerov <blaje@abv.bg>
2019-06-08 23:59:04 +0300

    added product footer

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,7 @@ import {ProductConsimer} from '../context';
 
 export default function Product(props) {
     console.log(props)
-   const {id, title, img, price, inCard} = props.product;
+   const {id, title, img, price, inCart} = props.product;
     return (
         <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
             <div className="card">
@@ -13,6 +13,25 @@ export default function Product(props) {
                     <Link to="/details" >
                     <img src={img} alt="product" className="card-img-top"/>
                     </Link>
+                    <button className="cart-btn" 
+                    disabled={inCart ? true : false}
+                    onClick={()=>{console.log('added to the cart')}}>
+                    {inCart ?(<p className="text-capitalize mb-0" disabled>
+                    { " " } in cart    
+                    </p>) : (
+                        <i className="fas fa-cart-plus" />
+                        )}
+                    </button>                    
+                </div>
+                {/* card footer */}
+                <div className="card-footer d-flex justify-content-between">
+                        <p className="align-self-center mb-0">
+                            {title}
+                        </p>
+                        <h5 className="text-blue font-italic mb-0">
+                            <span className="mr-1">$</span>
+                            {price}
+                        </h5>
                 </div>
             </div>
         </ProductWrapper>
