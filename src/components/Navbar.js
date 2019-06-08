f919f31b52bd59d5b0b6dcd56785c9ac5fe48249
BlagovestGerov <blaje@abv.bg>
2019-06-08 14:16:49 +0300

    Added navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import { ButtonContainer } from './Button';
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+        <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
         Creative Commons (Attribution 3.0 Unported);
         https://www.iconfinder.com/Makoto_msk  */}
@@ -16,7 +16,7 @@ export default function Navbar() {
         <ul className="navbar-nav align-items-center">
             <li className="nav-item ml-5">
                 <Link to="/" className="nav-link">
-                Poducts
+                products
                 </Link>
             </li>
         </ul>
@@ -28,7 +28,15 @@ export default function Navbar() {
             my cart
         </ButtonContainer>
         </Link>
-        </nav>
+        </NavWrapper>
     )
 }
 
+const NavWrapper = styled.nav`
+background:var(--mainBlue);
+.nav-link{
+    color:var(--mainWhite) !important;
+    font-size:1.3rem;
+    text-transform:capitalize;
+}
+`;
