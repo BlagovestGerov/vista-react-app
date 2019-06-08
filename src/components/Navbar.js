3593b3072d9ea43160636e10d58b87582a970ba4
BlagovestGerov <blaje@abv.bg>
2019-06-08 07:57:57 +0300

    separate the buttons styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
 import styled from 'styled-components'
+import { ButtonContainer } from './Button';
 
 export default function Navbar() {
     return (
@@ -31,22 +32,3 @@ export default function Navbar() {
     )
 }
 
-const ButtonContainer = styled.button`
-text-transform:capitalized;
-font-size:1.4rem;
-background:transparent;
-border:0.05rem solid var(--lightBlue);
-color:var(--lightBlue);
-border-radius:0.5rem;
-padding:0.2rem 0.5rem;
-cursor:pointer;
-margin:0.2rem 0.5rem 0.2rem 0;
-transition:all 0.5s ease-in-out;
-&:hover{
-    background:var(--lightBlue);
-    color:var(--mainBlue);
-}
-&:focus{
-    outline:none;
-}
-`
