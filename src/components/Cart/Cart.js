310cac7ebc0d7f7a1bcb66a9a7c8b7893df31490
BlagovestGerov <blaje@abv.bg>
2019-06-10 20:07:08 +0300

    send cart items to the cart page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,6 +3,7 @@ import Title from '../Title';
 import CartColumns from './CartColumns';
 import EmptyCart from './EmptyCart';
 import {ProductConsumer} from '../../context';
+import CartList from './CartList';
 
 export default function Cart() {
     return (
@@ -15,6 +16,7 @@ export default function Cart() {
                                 <React.Fragment>
                                 <Title name="your" title="cart" />
                                 <CartColumns />
+                                <CartList value={value}/>
                                 </React.Fragment>
                             )
                         }else{
